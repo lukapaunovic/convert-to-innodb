bff48a083bb89872ff261b40d891c85b174ba426
Luka Paunović <office@leo-host.com>
2022-04-27 00:23:00 +0200

    Update convert.sh

--- convert.sh
+++ convert.sh
@@ -3,9 +3,9 @@
 # Converts all MyISAM tables in all databases to InnoDB
 
 
-#DATABASES="db1 db2"     # Convert databases db1 and db2 only
+DATABASES="db1 db2"     # Convert databases db1 and db2 only
 
-DATABASES="ALL"         # Convert all databases
+#DATABASES="ALL"         # Convert all databases
 
 MYSQL_USER=root
 # Uncomment if you're not using ~/.my.cnf file (will receive "Warning: Using a password on the command line interface can be insecure" warnings)
@@ -27,20 +27,17 @@ if [ "$DATABASES" == "ALL" ] ; then
     DATABASES=$(echo "SHOW DATABASES" | $MYSQL_COMMAND | egrep -v '(performance_schema|information_schema|mysql)')
 fi
 
-function convert_db() {
-    echo Converting $1
-    # Check if the table is MyISAM (we don't want to convert InnoDB tables over and over again)
-    TABLES=$(echo "SELECT TABLE_NAME FROM information_schema.TABLES where TABLE_SCHEMA = '$1' and ENGINE = 'MyISAM'" | $MYSQL_COMMAND)
-    for TABLE in $TABLES ; do
-        echo Converting MyISAM $TABLE to InnoDB
-        echo "ALTER TABLE $TABLE ENGINE = INNODB" | $MYSQL_COMMAND $1
-    done
-    if [ "x$TABLES" = "x" ] ; then
-        echo No MyISAM tables found in $1 database
-    fi
-    echo
-}
 
 for DATABASE in $DATABASES ; do
-convert_db $DATABASE &
+    echo Converting $DATABASE
+    # Check if the table is MyISAM (we don't want to convert InnoDB tables over and over again)
+    TABLES=$(echo "SELECT TABLE_NAME FROM information_schema.TABLES where TABLE_SCHEMA = '$DATABASE' and ENGINE = 'MyISAM'" | $MYSQL_COMMAND)
+    for TABLE in $TABLES ; do
+        echo Converting MyISAM $TABLE to InnoDB
+        echo "ALTER TABLE $TABLE ENGINE = INNODB" | $MYSQL_COMMAND $DATABASE
+    done
+    if [ "x$TABLES" = "x" ] ; then
+        echo No MyISAM tables found in $DATABASE database
+    fi
+    echo
 done
